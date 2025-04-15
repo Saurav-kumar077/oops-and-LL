c44569952ee9cf20080f9bd4bf910ce8ed5d95e2
Saurav-kumar077 <126255878+Saurav-kumar077@users.noreply.github.com>
2025-04-15 14:13:33 +0530

    updated

--- Linkedlistintro.cpp
+++ Linkedlistintro.cpp
@@ -12,12 +12,38 @@ public:
         this->next = NULL;
     }
 };
-void inserthead(Node *&head, int d)
+ void inserthead(Node *&head, int d)
 {
     // creating new node
     Node *temp = new Node(d);
     temp->next = head;
     head = temp;
+}
+void insertTail(Node* &Tail ,int d){
+    Node * temp =new Node(d);
+    Tail -> next=temp;
+    Tail=temp;
+}
+void insertAtPosition( Node* &Tail ,Node * &head,int position ,int d){
+
+    if(position==1){
+        inserthead(head,d);
+        return;
+    }
+    Node *temp= head;
+    int count= 1;
+    while(count<position-1){
+        temp=temp->next;
+        count++;
+    }
+    if(temp->next==NULL){
+        insertTail(Tail,d);
+        return;
+    }
+    Node *NodeToInsert =new Node(d);
+    NodeToInsert ->next = temp->next;
+    temp ->next = NodeToInsert;
+
 }
 void print(Node *&head)
 {
@@ -35,8 +61,13 @@ int main()
     // cout<<Node1->data<<endl;
     // cout<<Node1->next<<endl;
     Node *head = Node1;
-    print(head);
-    inserthead(head, 12);
+    Node *Tail = Node1;
+    print(Tail);
+    insertTail(Tail, 12);
+    print(Tail);
+    insertTail(Tail,15);
+    print(Tail);
+    insertAtPosition(Tail,head,4,22);
     print(head);
     return 0;
 }
